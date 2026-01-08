a2166468a4ee36a1e65a1d51a8bdbf410cdd0f23
Nicolas Guerrero <Guerreronicolas1872@gmail.com>
2026-01-08 15:42:09 -0800

    Initial Commit - Set-up base application

---
+++ README.md
@@ -0,0 +1,12 @@
+# CTI Slack App: MyCTI
+Goal: Create a Slack Interface to grant data system access to students
+* Slack GUI where students can view and edit their records
+* Distribute notifications and reminder more programmatically
+
+### Development Notes:
+* Uses Slack Bolt (Python)
+  * Socket Mode is enabled. The app is meant to only be used in the existing Slack workspace, there's no interest in publishing this to the marketplace.
+
+
+# Request
+https://<your-domain>/slack/events
---
+++ app.py
@@ -0,0 +1,20 @@
+import os
+from dotenv import load_dotenv
+
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+from slack_bolt.adapter.flask import SlackRequestHandler
+
+load_dotenv()
+
+# Use bot token + socket handler
+app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
+
+# Respond to hello message
+@app.message("hello")
+def message_hello(message, say):
+    # say() sends a message to the channel where the event was triggered
+    say(f"Hey there <@{message['user']}>!")
+
+if __name__ == "__main__":
+    SocketModeHandler(app, os.environ.get("SLACK_APP_TOKEN")).start()
