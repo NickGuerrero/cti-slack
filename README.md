3e5b80e23d3cd2b7d2b076e35e8b8b81b25a9d15
Nicolas Guerrero <Guerreronicolas1872@gmail.com>
2026-01-09 15:56:07 -0800

    Add Flask functionality to run application in HTTP mode

--- README.md
+++ README.md
@@ -4,9 +4,11 @@ Goal: Create a Slack Interface to grant data system access to students
 * Distribute notifications and reminder more programmatically
 
 ### Development Notes:
-* Uses Slack Bolt (Python)
-  * Socket Mode is enabled. The app is meant to only be used in the existing Slack workspace, there's no interest in publishing this to the marketplace.
-
+* Uses Slack Bolt (Python + Flask + Gunicorn)
+  * The Flask adapter is used since the sample seems more up to date
+    * We could change adapters later (e.g. FastAPI), more research could be done to ensure compatability
+  * Gunicorn is currently configured with a basic sync worker
+    * Once we have a better idea on what works for Slack bolt, we can modify this too
 
 # Request
 https://<your-domain>/slack/events
--- app.py
+++ app.py
@@ -2,19 +2,25 @@ import os
 from dotenv import load_dotenv
 
 from slack_bolt import App
-from slack_bolt.adapter.socket_mode import SocketModeHandler
+# from slack_bolt.adapter.socket_mode import SocketModeHandler
 from slack_bolt.adapter.flask import SlackRequestHandler
+from flask import Flask, request
 
-load_dotenv()
+load_dotenv() # In Heroku, make sure values are set in config vars
 
 # Use bot token + socket handler
-app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
+slack_app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 
 # Respond to hello message
-@app.message("hello")
+@slack_app.message("hello")
 def message_hello(message, say):
     # say() sends a message to the channel where the event was triggered
     say(f"Hey there <@{message['user']}>!")
 
-if __name__ == "__main__":
-    SocketModeHandler(app, os.environ.get("SLACK_APP_TOKEN")).start()
+# Flask Handler Set-Up
+flask_app = Flask(__name__)
+handler = SlackRequestHandler(slack_app)
+
+@flask_app.route("/slack/events", methods=["POST"])
+def slack_events():
+    return handler.handle(request)
