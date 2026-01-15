fa0230c0e6a9ede4fd469386c6e69cee578b0047
ConlynPattison <conlynrp@gmail.com>
2026-01-14 23:26:51 -0800

    Add conditional socket adapter for local dev
    If run locally with `python app.py`, defaults to SocketMode.
    Running on Heroku will resolve to Flask adapter/handler.

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ import os
 from dotenv import load_dotenv
 
 from slack_bolt import App
-# from slack_bolt.adapter.socket_mode import SocketModeHandler
+from slack_bolt.adapter.socket_mode import SocketModeHandler
 from slack_bolt.adapter.flask import SlackRequestHandler
 from flask import Flask, request
 
 load_dotenv() # In Heroku, make sure values are set in config vars
 
-# Use bot token + socket handler
+# Initialize Slack app
 slack_app = App(
     token=os.environ.get("SLACK_BOT_TOKEN"),
     signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
@@ -20,10 +20,15 @@ def message_hello(message, say):
     # say() sends a message to the channel where the event was triggered
     say(f"Hey there <@{message['user']}>!")
 
-# Flask Handler Set-Up
+# Flask app (used in production)
 flask_app = Flask(__name__)
 handler = SlackRequestHandler(slack_app)
 
 @flask_app.route("/slack/events", methods=["POST"])
 def slack_events():
     return handler.handle(request)
+
+# Conditional launcher
+if __name__ == "__main__" and os.environ.get("SLACK_APP_TOKEN"):
+    # Development: Use SocketMode
+    SocketModeHandler(slack_app, os.environ.get("SLACK_APP_TOKEN")).start()
