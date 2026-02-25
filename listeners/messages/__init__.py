3d6239655e1aa2cdf3e37513efdaaf11f99cea54
ConlynPattison <conlynrp@gmail.com>
2026-02-24 22:20:43 -0800

    Revert health-check message back to message (was event)

--- listeners/messages/__init__.py
+++ listeners/messages/__init__.py
@@ -3,4 +3,4 @@ from .hello_message import message_hello_callback
 
 
 def register(app: App):
-    app.event("message")(message_hello_callback)
+    app.message("hello")(message_hello_callback)
--- listeners/messages/hello_message.py
+++ listeners/messages/hello_message.py
@@ -3,6 +3,5 @@ from slack_bolt import Say
 
 def message_hello_callback(event, say: Say):
     # say() sends a message to the channel where the event was triggered
-    if event.get("text", "").lower() == "hello":
-        say(f"Hey there <@{event['user']}>!")
+    say(f"Hey there <@{event['user']}>!")
         
